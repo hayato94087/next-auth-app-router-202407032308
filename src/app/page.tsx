8d15a67ff60057f0240c5daf4a49ca67e5fe4375
hayato94087 <hayato94087@gmail.com>
2024-07-02 21:33:41 +0900

    1.2 shadcn/ui の導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,16 @@
 import { type FC } from "react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+ } from "@/components/ui/alert-dialog";
+
+import { Button } from "@/components/ui/button";
 
 const Home: FC = () => {
   return (
@@ -6,10 +18,33 @@ const Home: FC = () => {
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World</h1>
+          <div className="h-4" />
+          <HelloWorldButton />
         </div>
       </div>
     </main>
   );
 };
+
+const HelloWorldButton = () => {
+  return (
+    <AlertDialog defaultOpen={false}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Click Me</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Hello World</AlertDialogTitle>
+          <AlertDialogDescription>
+            これは単純な「Hello World」のダイアログです。
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>閉じる</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+ };
 
 export default Home;
